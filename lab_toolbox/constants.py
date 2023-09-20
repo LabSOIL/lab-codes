237273223dfcb4f7cdbe63949bfed4c3baec5943
Evan Thomas <evan.thomas@epfl.ch>
2023-09-20 15:56:00 +0200

    Modify code to allow dynamic columns (plot rendering, header identification), add tests, add extra dataset with 6 columns and irregular column numbers (missing i4, i8)

--- lab_toolbox/constants.py
+++ lab_toolbox/constants.py
@@ -3,17 +3,17 @@ from pydantic import BaseSettings
 
 class Constants(BaseSettings):
     DEFAULT_POINT_SIZE: int = 0
-    DEFAULT_POINT_COLOUR: str = '#a3a7e4'
-    INTEGRAL_PEAK_COLOUR: str = '#FF6692'
+    DEFAULT_POINT_COLOUR: str = "#a3a7e4"
+    INTEGRAL_PEAK_COLOUR: str = "#FF6692"
     INTEGRAL_PEAK_SIZE: int = 20
 
-    SELECTED_POINT_COLOUR: str = '#2F4F4F'
+    SELECTED_POINT_COLOUR: str = "#2F4F4F"
     SELECTED_POINT_SIZE: int = 20
-    FILENAME_RAW_DATA: str = 'RawDatOut_301_b.csv'
-    FILENAME_BASELINE_SUBTRACTED_DATA: str = 'bs_301_b.csv'
-    FILENAME_SUMMARY_DATA: str = 'Output_301_b.csv'
+    FILENAME_RAW_DATA: str = "RawDatOut_301_b.csv"
+    FILENAME_BASELINE_SUBTRACTED_DATA: str = "bs_301_b.csv"
+    FILENAME_SUMMARY_DATA: str = "Output_301_b.csv"
 
-    FILE_HEADER: str = 'Time/s, i1/A, i2/A, i3/A, i4/A, i5/A, i6/A, i7/A, i8/A'
+    FILE_HEADER: str = "Time/s, "
 
 
 constants = Constants()
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,41 +3,51 @@ import pytest
 import os
 
 
-RESOURCES_DIR = os.path.join(os.getcwd(), 'tests', 'resources')
+RESOURCES_DIR = os.path.join(os.getcwd(), "tests", "resources")
 
 
 @pytest.fixture(scope="session")
 def matlab_integral_data():
-    ''' Read integral data from MATLAB outputs '''
+    """Read integral data from MATLAB outputs"""
 
-    df = pd.read_csv(
-        os.path.join(RESOURCES_DIR, 'matlab_integral_output.csv')
-    )
-    df.set_index('measurement', inplace=True)
+    df = pd.read_csv(os.path.join(RESOURCES_DIR, "matlab_integral_output.csv"))
+    df.set_index("measurement", inplace=True)
 
     return df
 
 
 @pytest.fixture(scope="session")
-def example_input_data():
-    ''' Read example input data '''
+def example_input_data_8col():
+    """Read example input data"""
 
     df = pd.read_csv(
-        os.path.join(RESOURCES_DIR, 'Example_input.txt'),
-        header=25
+        os.path.join(RESOURCES_DIR, "Example_input_8col.txt"), header=25
     )
     # df.set_index('Time/s', inplace=True)  # Set index to the time column
-    df.columns = df.columns.str.strip()   # Strip whitespace from the columns
+    df.columns = df.columns.str.strip()  # Strip whitespace from the columns
+
+    return df
+
+
+@pytest.fixture(scope="session")
+def example_input_data_6col():
+    """Read example input data"""
+
+    df = pd.read_csv(
+        os.path.join(RESOURCES_DIR, "Example_input_6col.txt"), header=62
+    )
+    # df.set_index('Time/s', inplace=True)  # Set index to the time column
+    df.columns = df.columns.str.strip()  # Strip whitespace from the columns
 
     return df
 
 
 @pytest.fixture(scope="session")
 def example_baseline_filtered_data():
-    ''' Read example baseline data '''
+    """Read example baseline data"""
 
     df = pd.read_csv(
-        os.path.join(RESOURCES_DIR, 'matlab_baseline_filtered.csv')
+        os.path.join(RESOURCES_DIR, "matlab_baseline_filtered.csv")
     )
 
     return df
